a79e99fc0ecd672fd78233fb5736baf5e0d5071d
awolf-lib <andywol93@gmail.com>
2021-02-16 13:20:14 +0100

    1

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -2,37 +2,30 @@ package EG::GenomeHubs::SiteDefs;
 
 use strict;
 
-sub update_conf {
-  push @$SiteDefs::ENSEMBL_API_LIBS, $SiteDefs::ENSEMBL_SERVERROOT . '/template-plugin/modules';
 
-  $SiteDefs::SITE_LOGO = 'mealybugbase.png';
+sub update_conf {
+  push @$SiteDefs::ENSEMBL_API_LIBS, $SiteDefs::ENSEMBL_SERVERROOT . '/gh-ensembl-plugin/modules';
+
+  $SiteDefs::SITE_LOGO = 'ensembl.genomehubs.png';
   $SiteDefs::SITE_LOGO_WIDTH = 150;
   $SiteDefs::SITE_LOGO_HEIGHT = 50;
-  $SiteDefs::SITE_LOGO_ALT = 'Mealybug Ensembl';
-  $SiteDefs::SITE_NAME = 'Test Ensembl';
+  $SiteDefs::SITE_LOGO_ALT = 'GenomeHubs Ensembl';
+  $SiteDefs::SITE_NAME = 'GenomeHubs Ensembl';
 
   $SiteDefs::ISSUE_TRACKER_URL = 'https://github.com/genomehubs/genomehubs/issues?status=new&status=open';
-  $SiteDefs::ISSUE_TRACKER_TITLE = 'Report an issue';
+  $SiteDefs::ISSUE_TRACKER_TITLE = 'report an issue';
 
   $SiteDefs::ENSEMBL_SERVERADMIN = 'contact&#064;lepbase.org';
 
-  $SiteDefs::ASSEMBLY_GROUP_A = [
-          'Trichinella_nelsoni_tnel1',
-          'Trichinella_papuae_tpap1',
-          'Trichinella_patagoniensis_tpat1'
-  ];
-  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Trichinella';
+#  $SiteDefs::ASSEMBLY_GROUP_A = ['Melitaea_cinxia'];
+  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Assemblies with gene models';
   $SiteDefs::ASSEMBLY_GROUP_A_TEMPLATE = '_fav_template';
-  $SiteDefs::ASSEMBLY_GROUP_B = [
-          'Anisakis_simplex_asim1',
-	  'Brugia_timori_btim1',
-          'Dracunculus_medinensis_dmed1',
-	  'Globodera_pallida_gpal1',
-	  'Steinernema_glaseri_sgla1',
-	  'Steinernema_scapterisci_ssca1',
-  ];
-  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'rest';
+#  $SiteDefs::ASSEMBLY_GROUP_B = ['Bombyx_mori'];
+  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Assembly only';
   $SiteDefs::ASSEMBLY_GROUP_B_TEMPLATE = '_list_template';
+  $SiteDefs::ASSEMBLY_GROUP_C_TITLE = 'Other Assemblies';
+  $SiteDefs::ASSEMBLY_GROUP_C_TEMPLATE = '_list_template';
 }
+
 
 1;
