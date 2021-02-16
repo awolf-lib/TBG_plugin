b45089dd42c17ca49d23dd6ecab3e2e6950ca21f
awolf-lib <andywol93@gmail.com>
2021-02-16 15:44:41 +0100

    1

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -20,10 +20,10 @@ sub update_conf {
   $SiteDefs::ASSEMBLY_GROUP_A = ['Trichinella_nelsoni_tnel1'];
   $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Trichinella';
   $SiteDefs::ASSEMBLY_GROUP_A_TEMPLATE = '_fav_template';
-  $SiteDefs::ASSEMBLY_GROUP_B = [ ];
+  $SiteDefs::ASSEMBLY_GROUP_B = ['Trichinella_nelsoni_tnel1'];
   $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Assembly only';
   $SiteDefs::ASSEMBLY_GROUP_B_TEMPLATE = '_list_template';
-  $SiteDefs::ASSEMBLY_GROUP_C = [ ];
+  $SiteDefs::ASSEMBLY_GROUP_C = ['Trichinella_nelsoni_tnel1'];
   $SiteDefs::ASSEMBLY_GROUP_C_TITLE = 'Other Assemblies';
   $SiteDefs::ASSEMBLY_GROUP_C_TEMPLATE = '_list_template';
 }
