cf76402bdb480d4207a0646ddddd8a6dabd34651
awolf-lib <andywol93@gmail.com>
2021-02-23 13:51:19 +0100

    1

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -6,40 +6,24 @@ use strict;
 sub update_conf {
   push @$SiteDefs::ENSEMBL_API_LIBS, $SiteDefs::ENSEMBL_SERVERROOT . '/ensembl.mealybug/modules';
 
-  $SiteDefs::SITE_LOGO = 'mealybugbase.png';
-  $SiteDefs::SITE_LOGO_WIDTH = 150;
-  $SiteDefs::SITE_LOGO_HEIGHT = 50;
-  $SiteDefs::SITE_LOGO_ALT = 'Mealybug Ensembl';
-  $SiteDefs::SITE_NAME = 'Mealybug Ensembl';
-
-  $SiteDefs::ISSUE_TRACKER_URL = 'https://github.com/genomehubs/genomehubs/issues?status=new&status=open';
-  $SiteDefs::ISSUE_TRACKER_TITLE = 'report an issue';
-
-  $SiteDefs::ENSEMBL_SERVERADMIN = 'contact&#064;lepbase.org';
-
   $SiteDefs::ASSEMBLY_GROUP_A = [
-          'Planococcus_citri_pcitriv1',
-          'Planococcus_ficus_pficusv0',
-          'Pseudococcus_longispinus_v1'
+      "melitaea_cinxia_core_40_93_1",
+      "trichinella_nelsoni_tnel1_core_40_93_1",
+      "trichinella_papuae_tpap1_core_40_93_1",
+      "trichinella_patagoniensis_tpat1_core_40_93_1"
   ];
-  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Mealybug assemblies';
+  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Trichinella assemblies';
   $SiteDefs::ASSEMBLY_GROUP_A_TEMPLATE = '_fav_template';
   $SiteDefs::ASSEMBLY_GROUP_B = [
-          'Maconellicoccus_hirsutus_mhir1',
-          'Ferrisia_virgata_fvir1',
-          'Paracoccus_marginatus_pmar1',
-          'Trionymus_perrisii_tper1'
+      "dracunculus_medinensis_dmed1_core_40_93_1",
+      "steinernema_scapterisci_ssca1_core_40_93_1",
+      "steinernema_glaseri_sgla1_core_40_93_1",
+      "anisakis_simplex_asim1_core_40_93_1",
+      "globodera_pallida_gpal1_core_40_93_1",
+      "brugia_timori_btim1_core_40_93_1",
   ];
-  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Mealybug assemblies without gene models';
+  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Others';
   $SiteDefs::ASSEMBLY_GROUP_B_TEMPLATE = '_list_template';
-  $SiteDefs::ASSEMBLY_GROUP_C = [
-          'Acyrthosiphon_pisum',
-          'Bemisia_tabaci_MEAM1_v1x1',
-          'Rhodnius_prolixus',
-          'Myzus_persicae_CloneG006_v2'
-  ];
-  $SiteDefs::ASSEMBLY_GROUP_C_TITLE = 'Other Hemiptera';
-  $SiteDefs::ASSEMBLY_GROUP_C_TEMPLATE = '_fav_template';
 }
 
 
