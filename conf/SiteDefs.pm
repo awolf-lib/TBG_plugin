c5da95631e0330572ea7f9789c99a3498b5f73f3
Andreas Wolf <54278991+awolf-lib@users.noreply.github.com>
2021-03-03 13:50:59 +0100

    Update SiteDefs.pm

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -1,29 +1,74 @@
-package EG::GenomeHubs::SiteDefs;
+package EG::TBG::SiteDefs;
 
 use strict;
 
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 MODIFICATIONS
+
+Copyright [2014-2015] University of Edinburgh
+
+All modifications licensed under the Apache License, Version 2.0, as above.
+
+=cut
+
+package EG::TBG::SiteDefs;
+use strict;
+
 sub update_conf {
-  push @$SiteDefs::ENSEMBL_API_LIBS, $SiteDefs::ENSEMBL_SERVERROOT . '/TBG_plugin/modules';
+    $SiteDefs::ENSEMBL_PORT           = 8080;
 
-  $SiteDefs::ASSEMBLY_GROUP_A = [
-      "melitaea_cinxia_core_40_93_1",
-      "trichinella_nelsoni_tnel1_core_40_93_1",
-      "trichinella_papuae_tpap1_core_40_93_1",
-      "trichinella_patagoniensis_tpat1_core_40_93_1"
-  ];
-  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Trichinella assemblies';
-  $SiteDefs::ASSEMBLY_GROUP_A_TEMPLATE = '_fav_template';
-  $SiteDefs::ASSEMBLY_GROUP_B = [
-      "dracunculus_medinensis_dmed1_core_40_93_1",
-      "steinernema_scapterisci_ssca1_core_40_93_1",
-      "steinernema_glaseri_sgla1_core_40_93_1",
-      "anisakis_simplex_asim1_core_40_93_1",
-      "globodera_pallida_gpal1_core_40_93_1",
-      "brugia_timori_btim1_core_40_93_1",
-  ];
-  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Others';
-  $SiteDefs::ASSEMBLY_GROUP_B_TEMPLATE = '_list_template';
+## BEGIN LEPBASE MODIFICATIONS...
+    $SiteDefs::ENSEMBL_SERVERNAME     = 'ghubs.izn-ffm.intern';
+## ...END LEPBASE MODIFICATIONS
+
+    map {delete($SiteDefs::__species_aliases{$_}) } keys %SiteDefs::__species_aliases;
+
+## BEGIN LEPBASE MODIFICATIONS...
+    $SiteDefs::ENSEMBL_PRIMARY_SPECIES  = 'Heliconius_melpomene_hmel2'; # Default species
+    $SiteDefs::ENSEMBL_SECONDARY_SPECIES  = 'Melitaea_cinxia'; # Default species
+
+    $SiteDefs::__species_aliases{ 'Trichinella_nelsoni' }           = [qw(trichinella_nelsoni_tnel1_core_40_93_1)];
+
+    $SiteDefs::__species_aliases{ 'Dracunculus_medinensis' }= [qw(dracunculus_medinensis_dmed1_core_40_93_1)];
+
+
+## ...END LEPBASE MODIFICATIONS
+    @SiteDefs::ENSEMBL_PERL_DIRS    = (
+                                           $SiteDefs::ENSEMBL_WEBROOT.'/perl',
+                                           $SiteDefs::ENSEMBL_SERVERROOT.'/eg-web-common/perl',
+                                           $SiteDefs::ENSEMBL_SERVERROOT.'/eg-web-metazoa/perl',
+				       );
+
+## BEGIN LEPBASE MODIFICATIONS...
+    $SiteDefs::SITE_NAME = 'TBG';
+    $SiteDefs::ENSEMBL_SITETYPE = 'TBG';
+## ...END LEPBASE MODIFICATIONS
+    $SiteDefs::SITE_FTP= 'ftp://ftp.ensemblgenomes.org/pub/metazoa';
+    push @SiteDefs::ENSEMBL_HTDOCS_DIRS,  $SiteDefs::ENSEMBL_SERVERROOT.'/../biomarts/metazoa/biomart-perl/htdocs';
+    
+    $SiteDefs::DOCSEARCH_INDEX_DIR = $SiteDefs::ENSEMBL_SERVERROOT.'/eg-web-metazoa/data/docsearch';
+
+    $SiteDefs::ENA_COLLECTION_ID = 223;
+
+    $SiteDefs::ENA_SAMPLE_SEQ = "MSLKPKIVEFVDVWPRLRCIAESVITLTKVERSVWNTSFSDVYTLCVAQPEPMADRLYGETKHFLEQHVQEMLAKKVLIEGECSHSNGGPDLLQRYYITWMEYSQGIKYLHQLYIYLNQQHIKKQKITDTESFYGNLSSDAAEQMEIGELGLDIWRLYMIEYLSSELVRHILEGIAADRASNGTLDHHRVQIINGVIHSFVEVQDYKKTGSLKLYQELFEGPMLEASGAYYTDEANKLLHRCSVSEYMQEVIRILEYESRRAQKFLHVSSLPKLRKECEEKFINDRLGFIYSECREMVSEERRQDLRNMYVVLKPIPDNLKSELITTFLDHIKSEGLQTVSALKGENIHIAFVENMLKVHHKYQELIADVFENDSLFLSALDKACASVINRRPTERQPCRSAEYVAKYCDTLLKKSKTCEAEIDQKLTNNITIFKYIEDKDVYQKFYSRLLAKRLIHEQSQSMDAEEGMINRLKQACGYEFTNKLHRMFTDISVSVDLNNKFNTHLKDSNVDLGINLAIKVLQAGAWPLGSTQVIPFAVPQEFEKSIKMFEDYYHKLFSGRKLTWLHHMCHGELKLSHLKKSYIVTMQTYQMAIILLFETCDSLSCREIQNTLQLNDETFQKHMQPIIESKLLNASSENLAGETRIELNLDYTNKRTKFK";
+
 }
-
 
 1;
