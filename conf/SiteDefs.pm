22afcc8af903fe09f6669bf679c5bc1e2990ef01
awolf-lib <andywol93@gmail.com>
2021-03-31 08:21:32 +0000

    test

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -6,23 +6,34 @@ use strict;
 sub update_conf {
   push @$SiteDefs::ENSEMBL_API_LIBS, $SiteDefs::ENSEMBL_SERVERROOT . '/TBG_plugin/modules';
 
-  $SiteDefs::SITE_LOGO = 'ensembl.genomehubs.png';
+  $SiteDefs::SITE_LOGO = 'mealybugbase.png';
   $SiteDefs::SITE_LOGO_WIDTH = 150;
   $SiteDefs::SITE_LOGO_HEIGHT = 50;
-  $SiteDefs::SITE_LOGO_ALT = 'GenomeHubs Ensembl';
-  $SiteDefs::SITE_NAME = 'GenomeHubs Ensembl';
+  $SiteDefs::SITE_LOGO_ALT = 'Mealybug Ensembl';
+  $SiteDefs::SITE_NAME = 'Mealybug Ensembl';
 
   $SiteDefs::ISSUE_TRACKER_URL = 'https://github.com/genomehubs/genomehubs/issues?status=new&status=open';
   $SiteDefs::ISSUE_TRACKER_TITLE = 'report an issue';
 
   $SiteDefs::ENSEMBL_SERVERADMIN = 'contact&#064;lepbase.org';
 
-  $SiteDefs::ASSEMBLY_GROUP_A = ['Melitaea_cinxia'];
-  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'TBG Assemblies';
+  $SiteDefs::ASSEMBLY_GROUP_A = [
+          'Trichinella_nelsoni_tnel1'
+  ];
+  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Mealybug assemblies';
   $SiteDefs::ASSEMBLY_GROUP_A_TEMPLATE = '_fav_template';
-  $SiteDefs::ASSEMBLY_GROUP_B = ['Bombyx_mori'];
-  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Assembly only';
+  $SiteDefs::ASSEMBLY_GROUP_B = [
+          
+  ];
+  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Mealybug assemblies without gene models';
   $SiteDefs::ASSEMBLY_GROUP_B_TEMPLATE = '_list_template';
-  $SiteDefs::ASSEMBLY_GROUP_C_TITLE = 'Other Assemblies';
-  $SiteDefs::ASSEMBLY_GROUP_C_TEMPLATE = '_list_template';
+  $SiteDefs::ASSEMBLY_GROUP_C = [
+            
+  ];
+  $SiteDefs::ASSEMBLY_GROUP_C_TITLE = 'Other Hemiptera';
+  $SiteDefs::ASSEMBLY_GROUP_C_TEMPLATE = '_fav_template';
+}
+
+
+1;
 }
